02f56a6a57e93df6a418cbf7b6e4ef79b88dbfe8
Igor Spasic <igor.spasic@gmail.com>
2025-09-28 22:00:31 +0200

    Numbers and bundle icons

--- NumberQuest/NumberQuestApp.swift
+++ NumberQuest/NumberQuestApp.swift
@@ -1,12 +1,8 @@
-//
-//  NumberQuestApp.swift
-//  NumberQuest
-//
-//  Created by Igor Spasic on 11. 9. 2025..
-//
-
 import SwiftUI
 //import SwiftData
+
+let appVersion: String = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String ?? "1.0"
+let buildNumber: String = Bundle.main.infoDictionary?["CFBundleVersion"] as? String ?? "1"
 
 @main
 struct NumberQuestApp: App {
